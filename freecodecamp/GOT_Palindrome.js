b10aadcbe34eed5ceb54fa44edb8bda6891311b1
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-17 22:38:11 +0530

    pangram completed

--- freecodecamp/GOT_Palindrome.js
+++ freecodecamp/GOT_Palindrome.js
@@ -1,14 +1,25 @@
-function pangrams(s) {
-  const result = new Set();
-  for (const char of s.toUpperCase()) {
-    let pattern = /[A-Z]/;
-    if (pattern.test(char)) {
-      result.add(char);
-    }
-  }
-  return result.size === 26;
-}
+// function gameOfThrones(s) {
+//   let charCount = {};
+//   let oddCount = 0;
+//   for (let char of s) {
+//     charCount[char] = charCount[char] + 1 || 1;
+//   }
+//   for (let count of Object.values(charCount)) {
+//     if (count % 2 !== 0) oddCount++;
+//   }
+//   return oddCount <= 1 ? "YES" : "NO";
+// }
+// console.log(gameOfThrones("aaaaabbb"));
 
-console.log(
-  pangrams("We promptly judged antique ivory buckles for the next prize")
-);
+function gameOfThrones(s) {
+  let charCount = new Array(26).fill(0);
+  let oddCount = 0;
+  for (let char of s) {
+    charCount[char.charCodeAt() - "a".charCodeAt()]++;
+  }
+  for (let count of charCount) {
+    if (count % 2 !== 0) oddCount++;
+  }
+  return oddCount <= 1 ? "YES" : "NO";
+}
+console.log(gameOfThrones("aaaaabbb"));
