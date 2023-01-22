310c59a28c66fbfa2feb2565ff8237bcfdcc90d7
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-22 22:58:00 +0530

    use state hook done

--- NetNinjaReact/dojo-blog/src/components/Home.js
+++ NetNinjaReact/dojo-blog/src/components/Home.js
@@ -1,17 +1,25 @@
+import {useState} from 'react';
+
+
 const Home = () => {
+    const [name, setName] = useState("Amisha");
+  
+
   const handleClick = () => {
     alert("Hello Bro!");
   };
 
   const handleClickAgain = (name) => {
     alert("Hello Again" + name);
+    setName('Prathyanga');
   };
   return (
     <div className="home">
       <h3>Home</h3>
+      <h2>{name}</h2>
       <button onClick={handleClick}>Click Me</button>
 
-      //Wraps the handle function inside another function to pass parameters
+      {/* Wraps the handle function inside another function to pass parameters */}
       <button onClick={() => handleClickAgain(" Bro!")}>Click Me Again</button>
     </div>
   );
