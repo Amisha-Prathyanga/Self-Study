27f7a1ca2a11bf7182c839cbebee9dca382a7d3a
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-24 21:56:15 +0530

    Router link

--- NetNinjaReact/dojo-blog/src/App.js
+++ NetNinjaReact/dojo-blog/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./components/Create";
+import Navbar from "./components/Navbar";
 //Switch is removed from version 6 rrd
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <div className="content"></div>
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
+        <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
