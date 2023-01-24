73f5b5c73ad978efdf3162084faa7e9988caa256
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-24 20:41:47 +0530

    react router 1

--- NetNinjaReact/dojo-blog/src/App.js
+++ NetNinjaReact/dojo-blog/src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//Switch is removed from version 6 rrd
 
 function App() {
   const title = "Welcome to the dojo blog";
   const subtitle = "Enter at your own risk!";
   return (
-    <div className="App">
-      <div className="content"></div>
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content"></div>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <Routes>
+          <Route path="/" element={<Home />} >
+           </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
