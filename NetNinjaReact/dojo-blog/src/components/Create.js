3498f1c785e0f35653a3f9141cd17dfde998fae2
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-25 13:43:07 +0530

    controlled inputs done

--- NetNinjaReact/dojo-blog/src/components/Create.js
+++ NetNinjaReact/dojo-blog/src/components/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
 const Create = () => {
-    return (
-        <h1>Add New Blog</h1>
-      );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setauthor] = useState("mario");
+  return (
+    <div className="create">
+      <h1>Add New Blog</h1>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setauthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
