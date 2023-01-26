a2fe8e7845e33cb48b683782cbf02b095aa63f0b
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-26 23:36:33 +0530

    inheritence ex *not complete

---
+++ OOP_study/inheritance_Ex/ModArrayList.java
@@ -0,0 +1,10 @@
+package OOP_study.inheritance_Ex;
+
+import java.util.ArrayList;
+
+public class ModArrayList<D> extends ArrayList<D> {
+    public D getUsingMod(int index){
+        int validIndex = Math.abs(index) % this.size();
+        return this.get(validIndex);
+    }
+}
