8b64fbc29076814e657de8b91464700d90e3c479
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-27 23:34:40 +0530

    inheritance ex modified

--- OOP_study/inheritance_Ex/ModArrayList.java
+++ OOP_study/inheritance_Ex/ModArrayList.java
@@ -2,8 +2,9 @@ package OOP_study.inheritance_Ex;
 
 import java.util.ArrayList;
 
+//not fully completed
 public class ModArrayList<D> extends ArrayList<D> {
-    public D getUsingMod(int index){
+    public D getUsingMod(int index) {
         int validIndex = Math.abs(index) % this.size();
         return this.get(validIndex);
     }
