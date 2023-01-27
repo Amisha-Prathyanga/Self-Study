0fde0e7a4aca15dacf1ffbcd4c78e2d892fa864c
Amisha Prathyanga <spbamisha@gmail.com>
2023-01-28 00:10:55 +0530

    array done

---
+++ Data Structures/Array/Array.java
@@ -0,0 +1,10 @@
+package Data Structures.Array;
+
+public class Array {
+    
+        public void incrementArray(int[] input){
+            for(int i = 0; i< input.length; i++){
+                input[i]++;
+            }
+        }
+}
