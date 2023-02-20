b3a0b2c21dc52b4b36b2416f20fd57ba8e732533
Amisha Prathyanga <spbamisha@gmail.com>
2023-02-20 19:46:09 +0530

    palindrome completed without symbols

--- leetcode interview Q/string/palindrome.js
+++ leetcode interview Q/string/palindrome.js
@@ -1,16 +1,27 @@
 var isPalindrome = function (s) {
-  let arr = [];
+  //   let arr = [];
 
-  for (let i = 0; i < s.length; i++) {
-    if (!s[i].includes("," || ":")) {
-      arr.push(s[i]);
+  //   for (let i = 0; i < s.length; i++) {
+  //     if (!s[i].includes("," || ":")) {
+  //       arr.push(s[i]);
+  //     }
+  //   }
+
+  //   let joinedArr = arr.join("");
+
+  //   return joinedArr;
+
+  let joinedS = s.split(" ").join("").toLowerCase();
+  let reverseS = joinedS.split("").reverse().join("");
+
+  for (let i = 0; i < joinedS.length; i++) {
+    if (joinedS === reverseS) {
+      return "It is a palindrome!";
     }
   }
 
-  let joinedArr = arr.join("");
-
-  return joinedArr;
+  return "not a palindrome";
 };
 
-s = "A man, a plan, a canal: Panama";
+s = "racecar";
 console.log(isPalindrome(s));
